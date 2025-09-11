5fcd2b7fe675dc4b6e92dd54528881b150f6478c
Nahuel Anselmo <anselmonahuel63t@gmail.com>
2025-09-11 12:16:22 -0300

    Front de crud

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+
+export const App = () => {
+  return (
+    <>
+      <div>
+        <h1 className="text-4xl font-bold text-center">Hello World!</h1>
+        <p className="text-center mt-4">Welcome to my React App with Tailwind CSS</p>
+        </div>
+    </>
+  )
+}
