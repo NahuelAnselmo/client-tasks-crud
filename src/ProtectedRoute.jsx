9c6f3680d657167fe10e5cb8a9a744ba2d954754
Nahuel Anselmo <anselmonahuel63t@gmail.com>
2025-09-27 20:10:04 -0300

    Se agrego la logica de loading en authcontext

--- src/ProtectedRoute.jsx
+++ src/ProtectedRoute.jsx
@@ -2,9 +2,10 @@ import { Navigate, Outlet } from 'react-router-dom';
 import { useAuth } from './context/AuthContext';
 
 export const ProtectedRoute = () => {
-  const { user, isAuthenticated } = useAuth();
-
-  if (!isAuthenticated) {
+  const { loading, isAuthenticated } = useAuth();
+  
+  if (loading) return <h1>Loading...</h1>;
+  if (!loading && !isAuthenticated) {
     return <Navigate to='/login' replace />;
   }
 
--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,13 +1,15 @@
-import Cookies from 'js-cookie';
-import { createContext, useContext, useEffect, useState } from 'react';
-import { loginRequest, registerRequest } from '../api/auth';
+import Cookies from "js-cookie";
+import { createContext, useContext, useEffect, useState } from "react";
+import { loginRequest, registerRequest, verityTokenRequet } from "../api/auth";
+import { set } from "zod";
+import { is } from "zod/v4/locales";
 
 export const AuthContext = createContext();
 
 export const useAuth = () => {
   const context = useContext(AuthContext);
   if (!context) {
-    throw new Error('useAuth must be used within an AuthProvider');
+    throw new Error("useAuth must be used within an AuthProvider");
   }
   return context;
 };
@@ -16,6 +18,7 @@ export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [isauthenticated, setIsAuthenticated] = useState(false);
   const [errors, setErrors] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     if (errors.length > 0) {
@@ -23,7 +26,9 @@ export const AuthProvider = ({ children }) => {
       return () => clearTimeout(timer);
     }
   }, [errors]);
-  const signup = async user => {
+
+  const signup = async (user) => {
+    setLoading(true);
     try {
       const res = await registerRequest(user);
       if (res.status === 200) {
@@ -38,9 +43,12 @@ export const AuthProvider = ({ children }) => {
       } else {
         setErrors([error.response.data.message]);
       }
+    } finally {
+      setLoading(false);
     }
   };
-  const signin = async user => {
+  const signin = async (user) => {
+    setLoading(true);
     try {
       const res = await loginRequest(user);
       console.log(res);
@@ -51,25 +59,41 @@ export const AuthProvider = ({ children }) => {
         return setErrors(error.response.data);
       }
       setErrors([error.response.data.message]);
+    } finally {
+      setLoading(false);
     }
   };
 
   useEffect(() => {
-    const token = Cookies.get('token');
-    if (token) {
-      console.log('Token actual:', token); 
-      setIsAuthenticated(true);
-    } else {
-      setIsAuthenticated(false);
-      setUser(null);
+    async function checkLogin() {
+      setLoading(true);
+      const token = Cookies.get("token");
+      if (token) {
+        try {
+          const res = await verityTokenRequet(token);
+          if (!res.data) {
+            setIsAuthenticated(false);
+            setUser(null);
+          } else {
+            setIsAuthenticated(true);
+            setUser(res.data);
+          }
+        } catch (error) {
+          setIsAuthenticated(false);
+          setUser(null);
+        }
+      }
+      setLoading(false);
     }
-  }, [user]);
+    checkLogin();
+  }, []);
 
   return (
     <AuthContext.Provider
       value={{
         signup,
         signin,
+        loading,
         user,
         isauthenticated,
         errors,
